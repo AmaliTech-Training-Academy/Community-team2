7368851587c3c1a10d593b85048e109f0290a033
SilasKumi <silaskumi4@gmail.com>
2026-03-12 09:53:00 +0000

    feat: Update logging aspect with additional annotations for enhanced functionality

--- backend/src/main/java/com/amalitech/communityboard/aop/LoggingAspect.java
+++ backend/src/main/java/com/amalitech/communityboard/aop/LoggingAspect.java
@@ -2,11 +2,15 @@ package com.amalitech.communityboard.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.AfterThrowing;
 import java.util.Arrays;
 
 @Aspect
